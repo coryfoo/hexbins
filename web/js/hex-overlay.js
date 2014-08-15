5ca52d44d9fff1845094ca1f1e2da35c28efa6d6
Cory Tucker <cory@radius.com>
2014-08-14 22:10:53 -0700

    Clear hexagons before loading data, add NProgress library

--- web/js/hex-overlay.js
+++ web/js/hex-overlay.js
@@ -1,30 +1,36 @@
-var HexBinOverlay = function(url) {
-  this.url = url; 
+var HexBinOverlay = function (url) {
+  this.url = url;
 };
 
 HexBinOverlay.prototype = new google.maps.OverlayView();
 
-HexBinOverlay.prototype.onAdd = function() {
-    google.maps.event.addListener(map, 'dragend', this.draw.bind(this));
+HexBinOverlay.prototype.onAdd = function () {
+  google.maps.event.addListener(map, 'dragend', this.draw.bind(this));
+
+  NProgress.configure({ parent: '#map' });
 };
 
-HexBinOverlay.prototype.onRemove = function() {
+HexBinOverlay.prototype.onRemove = function () {
   this.svg && this.svg.remove();
 };
 
-HexBinOverlay.prototype.draw = function() {
-    if ( this.drawDelay ) {
-        console.debug('cancelling timer');
-        clearTimeout(this.drawDelay);
-    }
+HexBinOverlay.prototype.draw = function () {
+  if (this.drawDelay) {
+    console.debug('cancelling timer');
+    clearTimeout(this.drawDelay);
+  }
 
-    this.drawDelay = setTimeout(loadDataForBounds.bind(null, this), 600);
+  this.drawDelay = setTimeout(loadDataForBounds.bind(null, this), 600);
 };
 
 function loadDataForBounds(overlay) {
+  if (overlay.svg) {
+    overlay.svg.remove();
+  }
+
   var params = {
-    zoom:overlay.getMap().getZoom(),
-    bounds:JSON.parse(overlay.getMap().getBounds()
+    zoom: overlay.getMap().getZoom(),
+    bounds: JSON.parse(overlay.getMap().getBounds()
         .toString()
         .replace(/"/g, '')
         .replace(/\(/g, '[')
@@ -33,89 +39,90 @@ function loadDataForBounds(overlay) {
 
   var args = [];
   for (k in params) {
-    args.push(k+'='+JSON.stringify(params[k]));
+    args.push(k + '=' + JSON.stringify(params[k]));
   }
 
   args.push('signals=[]');
 
-  d3.json(overlay.url+'?'+args.join('&'), handleHexbinData.bind(null, overlay));
+  NProgress.start();
+  d3.json(overlay.url + '?' + args.join('&'), handleHexbinData.bind(null, overlay));
 }
 
 function handleHexbinData(overlay, error, data) {
-    if ( error ) {
-        console.error("Failed to load data", error);
-        return;
-    }
+  NProgress.done();
 
-    var panes = overlay.getPanes();
-    var projection = overlay.getProjection();
-    var quantile = calculateQuantile(data.bins);
+  if (error) {
+    console.error("Failed to load data", error);
+    return;
+  }
 
-    if ( overlay.svg ) {
-        overlay.svg.remove();
-    }
+  var panes = overlay.getPanes();
+  var projection = overlay.getProjection();
+  var quantile = calculateQuantile(data.bins);
 
-    var samplePoint = getPoint(Object.keys(data.bins)[0]);
-    var projectionCoordinates = calculateProjectionCoordinates(data.binSize, projection, samplePoint);
-    var hexLineString = pointLineStringWithPointTranslation(projectionCoordinates);
+  var samplePoint = getPoint(Object.keys(data.bins)[0]);
+  var projectionCoordinates = calculateProjectionCoordinates(data.binSize, projection, samplePoint);
+  var hexLineString = pointLineStringWithPointTranslation(projectionCoordinates);
 
-    overlay.svg = d3.select(panes.overlayImage).selectAll('svg')
-        .data(Object.keys(data.bins));
+  overlay.svg = d3.select(panes.overlayImage).selectAll('svg')
+      .data(Object.keys(data.bins));
 
-    overlay.svg
-        .enter()
+  overlay.svg
+      .enter()
         .append('svg')
         .style('width', projectionCoordinates.x * 2 + 'px')
         .style('height', projectionCoordinates.y * projectionCoordinates.ratio * 2 + 'px')
-        .style("left", function(d) {
-            return projection.fromLatLngToDivPixel(getPoint(d)).x-projectionCoordinates.x + 'px';
+        .style("left", function (d) {
+          return projection.fromLatLngToDivPixel(getPoint(d)).x - projectionCoordinates.x + 'px';
         })
-        .style("top", function(d) {
-            return projection.fromLatLngToDivPixel(getPoint(d)).y-projectionCoordinates.y + 'px';
+        .style("top", function (d) {
+          return projection.fromLatLngToDivPixel(getPoint(d)).y - projectionCoordinates.y + 'px';
         })
         .attr("class", "Blues");
 
-    overlay.svg
-        .append("polygon")
-        .style('opacity', 0)
-        .attr("transform", "translate("+projectionCoordinates.x+")")
-        .attr("points", hexLineString)
-        .attr("class", function(d) { return "q" + quantile(data.bins[d]) + "-9"; })
-        .transition()
-            .duration(1000)
-            .style('opacity',.7);
+  overlay.svg
+      .append("polygon")
+      .style('opacity', 0)
+      .attr("transform", "translate(" + projectionCoordinates.x + ")")
+      .attr("points", hexLineString)
+      .attr("class", function (d) {
+        return "q" + quantile(data.bins[d]) + "-9";
+      })
+      .transition()
+      .duration(1000)
+      .style('opacity', .7);
 }
 
 function calculateQuantile(bins) {
-    var min = Infinity,
-        max = -Infinity;
+  var min = Infinity,
+      max = -Infinity;
 
-    Array.prototype.forEach.call(
-      Object.keys(bins), 
-      function(key) { 
+  Array.prototype.forEach.call(
+      Object.keys(bins),
+      function (key) {
         var value = bins[key];
-        if ( min > value ) min = value;
-        if ( max < value ) max = value;
+        if (min > value) min = value;
+        if (max < value) max = value;
       });
 
-    return d3.scale.quantile()
-        .domain([min, max])
-        .range(d3.range(9));
+  return d3.scale.quantile()
+      .domain([min, max])
+      .range(d3.range(9));
 }
 
 function calculateProjectionCoordinates(binSize, projection, point) {
-    var centerPixel = projection.fromLatLngToDivPixel(point);
+  var centerPixel = projection.fromLatLngToDivPixel(point);
 
-    var xDiffPixel = projection.fromLatLngToDivPixel(new google.maps.LatLng(point.lat(), point.lng() - binSize)).x;
-    var yDiffPixel = projection.fromLatLngToDivPixel(new google.maps.LatLng(point.lat() - binSize, point.lng())).y;
-    var xDiff = Math.abs(centerPixel.x - xDiffPixel) / 2;
-    var yDiff = Math.abs(centerPixel.y - yDiffPixel) / 2;
+  var xDiffPixel = projection.fromLatLngToDivPixel(new google.maps.LatLng(point.lat(), point.lng() - binSize)).x;
+  var yDiffPixel = projection.fromLatLngToDivPixel(new google.maps.LatLng(point.lat() - binSize, point.lng())).y;
+  var xDiff = Math.abs(centerPixel.x - xDiffPixel) / 2;
+  var yDiff = Math.abs(centerPixel.y - yDiffPixel) / 2;
 
-    return {
-        x: xDiff,
-        y: yDiff,
-        ratio: yDiff / xDiff
-    }
+  return {
+    x: xDiff,
+    y: yDiff,
+    ratio: yDiff / xDiff
+  }
 }
 
 function pointLineStringWithPointTranslation(projectionCoords) {
@@ -124,11 +131,11 @@ function pointLineStringWithPointTranslation(projectionCoords) {
 
   var points = [];
   points.push(0, 0);
-  points.push(projectionCoords.x,yLengthFromCenter*projectionCoords.ratio);
-  points.push(projectionCoords.x,(2*projectionCoords.y-yLengthFromCenter)*projectionCoords.ratio);
-  points.push(0, (2*projectionCoords.y)*projectionCoords.ratio);
-  points.push(-1*projectionCoords.x,(2*projectionCoords.y-yLengthFromCenter)*projectionCoords.ratio);
-  points.push(-1*projectionCoords.x,yLengthFromCenter*projectionCoords.ratio);
+  points.push(projectionCoords.x, yLengthFromCenter * projectionCoords.ratio);
+  points.push(projectionCoords.x, (2 * projectionCoords.y - yLengthFromCenter) * projectionCoords.ratio);
+  points.push(0, (2 * projectionCoords.y) * projectionCoords.ratio);
+  points.push(-1 * projectionCoords.x, (2 * projectionCoords.y - yLengthFromCenter) * projectionCoords.ratio);
+  points.push(-1 * projectionCoords.x, yLengthFromCenter * projectionCoords.ratio);
 
   return points.join(' ');
 }

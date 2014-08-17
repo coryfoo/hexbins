63f441c36401473106095b595d13b44413c2c7d8
Cory Tucker <cory@radius.com>
2014-08-17 00:45:36 -0700

    Tweaks to es verification

--- places/__init__.py
+++ places/__init__.py
@@ -4,8 +4,11 @@ from elasticsearch import Elasticsearch
 
 
 def verify_setup() -> bool:
-    resp = requests.get('http://localhost:9200/_scripts/groovy/hexbins')
-    return True if resp.status_code == 200 else False
+    try:
+        resp = requests.get('http://localhost:9200/_scripts/groovy/hexbins')
+        return True if resp.status_code == 200 else False
+    except:
+        return False
 
 
 def get_binned_places(bounds: list, bin_size: float, signals: list=None) -> list:
